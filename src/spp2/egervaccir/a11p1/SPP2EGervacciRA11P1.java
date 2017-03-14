d89ef6c8d515cc220fd27b9d8fab2d810c1564e2
usuario <usuario@home>
2017-03-14 13:39:36 -0600

    Actividad 11 Programa 1.00

---
+++ src/spp2/egervaccir/a11p1/SPP2EGervacciRA11P1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp2.egervaccir.a11p1;
+
+/**
+ *
+ * @author usuario
+ */
+public class SPP2EGervacciRA11P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventana2 window2 = new ventana2();
+        window2.show();
+        
+        ventana3 window3 = new ventana3();
+        window3.show();
+    }
+    
+}
